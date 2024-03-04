d4fad7718f78173657c8a2407acc84198bb69479
n0-f4ce <bologna.moloch_0p@icloud.com>
2024-03-03 19:57:54 -0800

    Refactor ieee80211_raw_frame_sanity_check function signature

--- esp32_marauder/src/WiFiScan.cpp
+++ esp32_marauder/src/WiFiScan.cpp
@@ -12,7 +12,7 @@ LinkedList<ssid>* ssids;
 LinkedList<AccessPoint>* access_points;
 LinkedList<Station>* stations;
 
-extern "C" int ieee80211_raw_frame_sanity_check(int32_t arg, int32_t arg2, int32_t arg3){
+static int ieee80211_raw_frame_sanity_check(int32_t arg, int32_t arg2, int32_t arg3){
     if (arg == 31337)
       return 1;
     else
